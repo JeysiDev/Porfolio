d8741e8e89548456b2bf6299b6e74bc385ba66c6
Jhon Carlo M. Ortega <ortegacarlo83@gmail.com>
2024-12-21 03:06:07 +0800

    contact-form.js index.html

--- js/components/contact-form.js
+++ js/components/contact-form.js
@@ -1,11 +1,17 @@
-function SendMail() {
+function SendMail(event) {
+    event.preventDefault(); // Prevent form from submitting normally
+    
     var params = {
-        from_name : document.getElementById("fullName").value,
-        email_id : document.getElementById("email_id").value,
-        subject : document.getElementById("subject").value,
-        message : document.getElementById("message").value
-    }
+        from_name: document.getElementById("fullName").value,
+        email_id: document.getElementById("email_id").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value
+    };
+
     emailjs.send("service_hnt20yh", "template_8njwlle", params).then(function (res) {
-        alert("Success! " + res.status);
-    })
+        alert("Success! Message sent.");
+        document.getElementById("contact-form").reset(); // Optionally reset the form
+    }).catch(function (error) {
+        alert("Failed to send message. Error: " + error);
+    });
 }
