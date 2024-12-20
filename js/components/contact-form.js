de0fdceb6b4e8a36fad883232fbf06207f3464ac
Jhon Carlo M. Ortega <ortegacarlo83@gmail.com>
2024-12-21 03:07:39 +0800

    contact-form.js

--- js/components/contact-form.js
+++ js/components/contact-form.js
@@ -3,7 +3,7 @@ function SendMail(event) {
     
     var params = {
         from_name: document.getElementById("fullName").value,
-        email_id: document.getElementById("email_id").value,
+        email_id: document.getElementById("email_id").value,   
         subject: document.getElementById("subject").value,
         message: document.getElementById("message").value
     };
